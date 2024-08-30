924abf666b05148d1a6f4bdc4311d2ce95630a4d
unknown <dimitras@thevtool.com>
2024-08-30 13:49:39 +0300

    This script instantiates all port correctly. Has been made simpler, since there is no need for checking every case. The splitting happens at the comma

--- top_script.py
+++ top_script.py
@@ -10,45 +10,38 @@ import argparse
 def find_ports(inst_file):
 
     module_name = None
-    ports = []        #It is a list, where input, output and inout will be stored.
-    parameters = []
+    ports       = []        #It is a list, where input, output and inout will be stored.
+    parameters  = []
     
     with open(inst_file, "r") as file:
         lines = file.readlines()     # Read the file and make each line an item in the list 'lines'
-        lines_iter = iter(lines)
+        lines_iter = iter(lines)     # Make it an iterator for the for-loop.
         for line in lines_iter:
-            line = line.strip()      # Strip the lines items from whitespace
-            if "module" in line:                                           # The re.search function searches the string line for 
-                match = re.search(r'module\s+(\w+)', line)                 # Search for module name. A match to the regex pattern provided as the first argument. In .group(1) the first capturing object
-                if match:                                                  # (as stated by the (\+w)) is saved. In this case that is the module name.
-                    module_name = match.group(1)                           # If there's a match, extract the module name
+            line = line.strip()      # Strip the lines items from whitespace.
+            if "module" in line:                              
+                match = re.search(r'module\s+(\w+)', line)  # Search for module name. A match to the regex pattern provided as the first argument. In .group(1), the first capturing object
+                if match:                                   # (as stated by the (\+w)) is saved. In this case, that is the module name.
+                    module_name = match.group(1)            # If there's a match, extract the module name. If not, module_name will be empty (executes line 46).
                     
             if "parameter" in line:                                        
-                param = re.findall(r'\b\w+\b', line.split("parameter")[-1])  
-                parameter_names = [param[0]]
-                parameters.extend(parameter_names)
-            
+                param = re.findall(r'\b\w+\b', line.split("parameter")[-1])  #line.split(): This splits the string line into a list of substrings, based on the word "parameter".
+                parameter_names = [param[0]]                                 #line.split()[-1] -> gives the last element of the list (the last substring, here everything that follows after "parameter").                                                  
+                parameters.extend(parameter_names)                           #re.findall -> finds all the words, so now param is a list of the words after the keyword "parameter"
+                                                                             #param[0] -> gives the first element, which is the parameter name.
             port_names   = ""
             
-            
-            if "input" in line or "output" in line or "inout" in line:    #Checks one condition after the other, not at the same time
-            #if ("input" or "output" or "inout") in line:  #If the parentheses is TRUE the if block is executed and obviously it doesn't run again. So the outputs and inouts are never written.
-            #if not "input" in line and not "output" in line and not "inout" in line: #This along with else : same result as the first if.
+            port_declaration = line.strip().startswith(("input", "output", "inout"))  # Is true only when a port is included in the design. If the line doesn't start with whistespace 
+            if port_declaration:                                                      # and the keyword input, output or inout, but does include them somewhere, then the port has been
+                                                                                      # commented out and should not be included in the instantiation.
                 port_section = ""
-            #else:
-                                                                                
-                    
-                                                                                
-                port_section = line.split("] ")[-1]                                             # [-1]: Retrieves the last element of the list resulting from the split operation.  
-                port_names = port_section.split()[0].strip(',')
-                #port_names = re.findall(r'\b(\w+)\s*(?:\[.*?\])?\s*(?:,\s*)?', port_section)   #line.split(): This splits the string line into a list of substrings,
-                if '//' in port_names:
-                    port_names = port_names.split('//')[0].strip()                                                                                
-                                                                                                  
-                #print(f"{port_names}")
-                ports.append(port_names)     # "ports" is a list 
-                                                                # .append(port_names): It's a method that adds all elements of port_names (which is a list) to the list ports. So, now the port names are all saved in the list. 
-                  
+                                                                        
+                port_section = line.split(",")[0]                 # Split the line at the comma and take the firts element (the substring before the comma).  
+                port_names = port_section.split()[-1]             # Split the line based on whitespace, so we have a list with the last element being the port_name. With the [-1] index, we extract it. 
+                                                                                                   
+      
+                ports.append(port_names)                # .append(port_names): It's a method that adds all elements of port_names (which is a list) to the list ports. 
+                                                        #  So, now the port names are all saved in the list. 
+       
                                                          
     if module_name is None:
         print("No module name found in the file. Please check the file content.")  
